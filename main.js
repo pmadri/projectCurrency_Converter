9892bbc96baaa910b4f1f855fe34d340c982f4fb
Pedro Aguiar <pedrophacv@gmail.com>
2025-05-30 20:29:25 -0300

    Realiza e mostra a conversão

--- main.js
+++ main.js
@@ -7,11 +7,21 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     const paraMoeda = document.getElementById('paraMoeda').value;
     
     //Definir taxas de cambio
-    const enchangeRates = {
+    const exchangeRates = {
         USD: { BRL: 5.73, EUR: 0.88 },
         BRL: { USD: 0.17, EUR: 0.15 },
         EUR: { USD: 1.13, BRL: 6.50 }
     };
 
+    let valorConvertido;
+    if (daMoeda === paraMoeda) {
+        valorConvertido = parseFloat(valor); // Se as moedas forem iguais, não há conversão
+    }else{
+        // Realizar a conversão
+        valorConvertido = parseFloat(valor) * exchangeRates[daMoeda][paraMoeda];
+    }
+
+    const conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda}`;
 
 });
